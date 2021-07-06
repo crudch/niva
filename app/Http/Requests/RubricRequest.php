64631da052a48f92f05ad022c84d353df8a867c6
Skiphog <skiphog@gmail.com>
2021-07-06 16:44:04 +0300

    prepare rubric admin

---
+++ app/Http/Requests/RubricRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RubricRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'slug' => Str::slug($this->input('slug') ?? $this->input('title')),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'min:3', 'max:100'],
+            'slug'  => [
+                'required',
+                'min:3',
+                'max:100',
+                Rule::unique('rubrics')
+            ]
+        ];
+    }
+}
--- app/Models/Rubric.php
+++ app/Models/Rubric.php
@@ -2,27 +2,35 @@
 
 namespace App\Models;
 
+use Orchid\Screen\AsSource;
+use Orchid\Filters\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * App\Models\Rubric
  *
- * @property int    $id
- * @property string $title
- * @property string $slug
+ * @property int                                                                 $id
+ * @property string                                                              $title
+ * @property string                                                              $slug
  * @method static \Illuminate\Database\Eloquent\Builder|Rubric newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Rubric newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Rubric query()
  * @mixin \Eloquent
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Article[] $articles
- * @property-read int|null $articles_count
+ * @property-read int|null                                                       $articles_count
  * @method static \Illuminate\Database\Eloquent\Builder|Rubric whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Rubric whereSlug($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Rubric whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rubric defaultSort(string $column, string $direction = 'asc')
+ * @method static \Illuminate\Database\Eloquent\Builder|Rubric filters(?\Orchid\Filters\HttpFilter $httpFilter = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rubric filtersApply(array $filters = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Rubric filtersApplySelection($selection)
  */
 class Rubric extends Model
 {
+    use AsSource, Filterable;
+
     /**
      * @var string
      */
@@ -34,6 +42,24 @@ class Rubric extends Model
     protected $fillable = [
         'title',
         'slug'
+    ];
+
+    /**
+     * Доступные поля для сортировки
+     *
+     * @var string[]
+     */
+    protected array $allowedSorts = [
+        'id',
+    ];
+
+    /**
+     * Доступные поля для фильтрации
+     *
+     * @var string[]
+     */
+    protected array $allowedFilters = [
+        'title'
     ];
 
     /**
